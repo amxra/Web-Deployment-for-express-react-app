48cd1e89a8cc05ac904e618064cb9f2af39fc720
amxra <amiradediran@gmail.com>
2019-10-31 10:52:03 +0100

    change package.json

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
+
+
 const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
 const friends = [
   { id: 1, name: 'Shaun' },
   { id: 2, name: 'Megan' },
